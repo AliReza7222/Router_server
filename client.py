5e1634a443ea3c0bc5c0e73e019b257a446cef9c
aliReza <ali.r.salehi99@gmail.com>
2021-10-13 23:57:26 +0330

    fix server and complate client

--- client.py
+++ client.py
@@ -1,2 +1,11 @@
 ###b_kh
 ###ali_coder
+import socket,pickle
+
+soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+start_id,end_id=map(int,input().split())
+id,port='127.0.0.1',72
+soc.connect( (id,port) )
+soc.send(bytes ( [start_id,end_id] ) )
+best_direction=pickle.loads(soc.recv(1024))
+#print(*best_direction)
--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 ###b_kh
 ###ali_coder
-import socket,threading
+import socket,threading,pickle
 from router import Router
+
 def Tread(client):
         router=Router('maps.txt')
-        start,end=(client.recv(1024)).decode()
-        best_direction=router.find_shortest_path( start,end )
-        client.send(bytes(best_direction))
+        start,end=tuple(client.recv(1024))
+        best_direction=pickle.dumps(router.find_shortest_path( start,end ))
+        client.send( best_direction )
         client.close()
 
-id,port="",72
+id,port="127.0.0.1",72
 soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 soc.bind((id,port))
 soc.listen(10)
+
 for _ in range(10):
     client,address=soc.accept()
-    threading.Thread(target=Tread,args=(client,))
+    threading.Thread(target=Tread,args=(client,)).start()
 soc.close()
