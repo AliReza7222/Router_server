aba585c5b71a39027a8be7d7f31a2b1c2964ef8e
aliReza <ali.r.salehi99@gmail.com>
2021-10-19 12:11:26 +0330

    complate map

--- map_file.py
+++ map_file.py
@@ -19,26 +19,25 @@ class run_map_file:
                     address_m[int(line[0])] = int(line[1]),int(line[2])
 
         picture = ig.imread("my_map.jpg")
-        print(self.best_direction)
         for number in range(len(self.best_direction)-1):
             x=[address_m[ self.best_direction [ number ] ][0],address_m[ self.best_direction [ number+1 ]][0] ]
             y=[address_m[ self.best_direction [ number ] ][1],address_m[ self.best_direction [ number+1 ]][1] ]
-            plt.plot(x,y,marker="o",color="red")
+            plt.plot(x,y,marker="o",color="#04b04d")
 
         for id in address_m:
             x,y= address_m[id][0],address_m[id][1]
-            plt.scatter(x,y,color="blue",s=25)
+            plt.scatter(x,y,color="#fa00d6",s=20,alpha=0.5)
             if id == 140 :
                 plt.annotate("Home",(x,y),fontsize=9,color="black")
             else:
-                plt.annotate(id,(x,y),fontsize=10,color="black")
+                plt.annotate(id,(x,y),fontsize=7,color="black")
         if len(self.best_direction) == 1:
             x,y=address_m[self.best_direction[0]][0],address_m[self.best_direction[0]][1]
             plt.scatter(x,y,color="red")
+
         plt.imshow(picture)
-        plt.title('map')
+        plt.title('Map')
         plt.xlabel("X")
         plt.ylabel("Y")
-        #plt.grid()
         plt.show()
 
