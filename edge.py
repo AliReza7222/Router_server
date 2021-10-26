f273af71527a2ffa37cf4747c538eee8f49f1915
aliReza <ali.r.salehi99@gmail.com>
2021-10-26 07:48:19 +0330

    clean code file

--- edge.py
+++ edge.py
@@ -1,17 +1,19 @@
-###b_kh
-###ali_coder
+# b_kh
+# ali_coder
 
 class Edge:
-    def __init__(self,head,tail):
-        self.__head=head
-        self.__tail=tail
+    def __init__(self, head, tail):
+        self.__head = head
+        self.__tail = tail
 
     def get_head(self):
         return self.__head
+
     def get_tail(self):
         return self.__tail
+
     def get_weight(self):
-        x1,x2=self.__head.get_x(),self.__tail.get_x()
-        y1,y2=self.__head.get_y(),self.__tail.get_y()
-        weight_edge= ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+        x1, x2 = self.__head.get_x(), self.__tail.get_x()
+        y1, y2 = self.__head.get_y(), self.__tail.get_y()
+        weight_edge = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
         return weight_edge
